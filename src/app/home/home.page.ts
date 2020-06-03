2f1939d9e8e4bc9326a5f147209c883e04ca5660
vini121423 <vinicius.santos838@etec.sp.gov.br>
2020-06-03 18:43:50 -0300

    Criação do aplicativo com foco no estudo da construção de variáveis e métodos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public mensagem = "Olá a todos(as)!";
 
-  constructor() {}
+  public nome: string; // Armazenar textos
+  public idade: number; // Armazenar numeros(inteiros,decimais);-
+  public excluido: boolean; // Armazenar true ou false
 
+  public situacao: any; // Armazena Qualquer tipo de variável
+
+  public anoNascimento: number;
+
+
+ // Constroi o que vai ser mostrado//
+  constructor() {
+    // alert(" Mensagem: " + this.mensagem );
+
+    // console.log(" Mensagem: " + this.mensagem );
+
+    // this.mensagem = 2020;
+
+  }
+
+  // Criar metodo 
+
+  public saudacao() {
+    this.mensagem = "Olá " + this.nome + " , você informou a idade de " + this.idade + " anos.";
+    if (this.idade >= 18) {
+      this.situacao = "Você é maior de idade!";
+    } else {
+      this.situacao = "Você é menor de idade!";
+    }
+  }
+
+  public anoDeNascimento() {
+    const anoAtual = 2020; // constante do ano não se altera
+    // cria variaveis que pode ter seu valor alterado durante a execução do programa
+    let anoNascimento = anoAtual - this.idade;
+
+
+    this.anoNascimento = anoNascimento;
+
+  }
+
+  /*
+ Data Binding (ligação entre Javascript e html)
+    1. Interpolação {{ valor }}
+     Realiza a "impressão" da varável no HTML em tempo real.
+    2. Two-Way Data Binding: [(ngModel)]="propriedade"
+     Atualiza a variável em tempo real.
+    3. Event Binding: (evento)="handler"
+     Executa o metodo a partir do evento
+    4. Property Binding: [propriedade]="valor"
+     
+    Associa a propriedade html a uma variável 
+
+  */
 }
